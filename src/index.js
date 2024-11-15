24d31e8261a29e9f86db43c71756450b06e21baa
OscarHV98 <oscar10hv.ohv@gmail.com>
2024-11-15 01:32:14 -0400

    sin force async

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import logger from "./logs/logger.js";
 const port = process.env.PORT;
 
 const main = async () => {
-    await sequelize.sync({force: true});
+    // await sequelize.sync({force: true});
+    await sequelize.sync();
     app.listen(port);
     console.log("server listening on port 3000");
     logger.info(`Server started on port${port}`);
